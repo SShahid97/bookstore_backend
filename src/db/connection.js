3deeba05a972e3ddd336c3485d8a19a1dcb8001e
Malik Shahid <sshahidmalik97@gmail.com>
2022-06-19 10:05:29 +0530

    connection string changes

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/bookStore")
-// mongoose.connect("mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0.ch92f.mongodb.net/bookStore?retryWrites=true&w=majority/bookStore")
+// mongoose.connect("mongodb://localhost:27017/bookStore")
+mongoose.connect(`mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0.ch92f.mongodb.net/bookStore?retryWrites=true&w=majority`)
 // mongoose.connect(`mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0.ch92f.mongodb.net/bookStore`)
 .then(()=>{
     console.log("Connection to MongoDB Successful");
